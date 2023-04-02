d8a789edf18a4fe65ffda1bc69ae9706285cdd38
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-04-02 03:29:46 -0400

    Using the Array Find Method

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+
+
+function superbowlWin(record){
+    
+    function findWin(record){
+        return record.result === 'W'
+    }
+        return record.find(findWin).year;
+
+}
+
+
+function superbowlWin2(record){
+    
+    function findWin(record){
+        return record.result === 'W'
+    }
+    if (findWin === true){
+        return record.find(findWin).year;
+    } else if (findWin !== true){
+    return undefined
+}
+}
